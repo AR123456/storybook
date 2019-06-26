761a0a6b321820cf58c38cb5f073ba79ae7e68c0
AR123456 <coloradoz99@gmail.com>
2019-06-26 06:17:28 -0600

    session and auth

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoURI:'mongodb://CHANGEME',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
-}
+  mongoURI:
+    "mongodb://coloradoz99:midnight1@ds056789.mlab.com:56789/anne-storybook-dev",
+  googleClientID:
+    "86480670993-qgvkc042npqo0g1onegimn4b67n8t3rv.apps.googleusercontent.com",
+  googleClientSecret: "H9c0DG5f3sNCUgExhHOHL3JU"
+};
