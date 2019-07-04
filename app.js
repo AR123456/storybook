2bc411730b11f6649db1a5c59f9e08b270919876
AR123456 <coloradoz99@gmail.com>
2019-07-04 07:04:57 -0600

    create google Strategy

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+// passport config
+require("./config/passport")(passport);
+//load routes
+const auth = require("./routes/auth");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It Works!');
+app.get("/", (req, res) => {
+  res.send("It Works!");
 });
 
+app.use("/auth", auth);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+  console.log(`Server started on port ${port}`);
 });
