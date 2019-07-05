6bead0c235538d4fa0248c5c21c16c5ddaea7ce0
AR123456 <coloradoz99@gmail.com>
2019-07-05 04:22:18 -0600

    user model

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 // passport config
 require("./config/passport")(passport);
+
 //load routes
 const auth = require("./routes/auth");
-
+//load Keys
+const keys = require("./config/keys");
+//mongoose Connect
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose
+  // if working with the depolyed app use
+  // .connect(keys.mongoURI, { useMongoClient: true })
+  .connect("mongodb://localhost/storybook", {
+    // useMongoClient: true
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
 const app = express();
 
 app.get("/", (req, res) => {
