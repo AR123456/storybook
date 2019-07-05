a4c7ce024669b7fa0a9ec0844300994f00c00ae1
AR123456 <coloradoz99@gmail.com>
2019-07-05 04:23:46 -0600

    adding sessionand auth

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const passport = require("passport");
-// passport config
+
+// Load User Model
+require("./models/User");
+
+// Passport Config
 require("./config/passport")(passport);
 
-//load routes
+// Load Routes
 const auth = require("./routes/auth");
-//load Keys
+
+// Load Keys
 const keys = require("./config/keys");
-//mongoose Connect
+
+// Map global promises
 mongoose.Promise = global.Promise;
-// Connect to mongoose
+// Mongoose Connect
+
 mongoose
-  // if working with the depolyed app use
+  // if dev localy could be
   // .connect(keys.mongoURI, { useMongoClient: true })
-  .connect("mongodb://localhost/storybook", {
-    // useMongoClient: true
-    useNewUrlParser: true
+  .connect(keys.mongoURI, {
+    useMongoClient: true
   })
-  .then(() => console.log("MongoDB Connected..."))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("It Works!");
 });
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Use Routes
 app.use("/auth", auth);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
