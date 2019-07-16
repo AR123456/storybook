ddd4c8748c5637ef0c555ba1852d79a8dc4c0afa
AR123456 <coloradoz99@gmail.com>
2019-07-16 06:26:41 -0600

    code along node js

--- app.js
+++ app.js
@@ -1,80 +1,46 @@
-const express = require("express");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const passport = require("passport");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
-// Load Models
-require("./models/User");
-require("./models/Story");
+// Load User Model
+require('./models/User');
 
 // Passport Config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Load Routes
-const index = require("./routes/index");
-const auth = require("./routes/auth");
-const stories = require("./routes/stories");
+const index = require('./routes/index');
+const auth = require('./routes/auth');
 
 // Load Keys
-const keys = require("./config/keys");
-
-// Handlebars Helpers
-const {
-  truncate,
-  stripTags,
-  formatDate,
-  select,
-  editIcon
-} = require("./helpers/hbs");
+const keys = require('./config/keys');
 
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
-mongoose
-  .connect(keys.mongoURI, {
-    useMongoClient: true
-  })
-  .then(() => console.log("MongoDB Connected"))
+mongoose.connect(keys.mongoURI, {
+  useMongoClient:true
+})
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 const app = express();
 
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// MEthod Override Middelware
-app.use(methodOverride("_method"));
-
 // Handlebars Middleware
-app.engine(
-  "handlebars",
-  exphbs({
-    helpers: {
-      truncate: truncate,
-      stripTags: stripTags,
-      formatDate: formatDate,
-      select: select,
-      editIcon: editIcon
-    },
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false
-  })
-);
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -86,16 +52,13 @@ app.use((req, res, next) => {
   next();
 });
 
-// Set static folder
-app.use(express.static(path.join(__dirname, "public")));
-
 // Use Routes
-app.use("/", index);
-app.use("/auth", auth);
-app.use("/stories", stories);
+app.use('/', index);
+app.use('/auth', auth);
+
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server started on port ${port}`)
 });
--- config/keys_prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
-};
-// on heroku add the keys   MONGO_URI,GOOGLE_CLIENT_ID, and GOOGLE_CLIENT_SECRET and their cooresponding values
