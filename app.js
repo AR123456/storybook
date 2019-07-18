9bc1b3b2054d0ee99cd07c732a51926a3f524cfa
AR123456 <coloradoz99@gmail.com>
2019-07-18 04:24:52 -0600

    dashboard and styes

--- app.js
+++ app.js
@@ -1,65 +1,67 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const passport = require('passport');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
 
 // Load Models
-require('./models/User');
-require('./models/Story');
+require("./models/User");
+require("./models/Story");
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // Load Routes
-const index = require('./routes/index');
-const auth = require('./routes/auth');
-const stories = require('./routes/stories');
+const index = require("./routes/index");
+const auth = require("./routes/auth");
+const stories = require("./routes/stories");
 
 // Load Keys
-const keys = require('./config/keys');
+const keys = require("./config/keys");
 
 // Handlebars Helpers
-const {
-  truncate,
-  stripTags,
-  formatDate
-} = require('./helpers/hbs');
+const { truncate, stripTags, formatDate } = require("./helpers/hbs");
 
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
-mongoose.connect(keys.mongoURI, {
-  useMongoClient:true
-})
-  .then(() => console.log('MongoDB Connected'))
+mongoose
+  .connect(keys.mongoURI, {
+    useMongoClient: true
+  })
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs({
-  helpers: {
-    truncate: truncate,
-    stripTags: stripTags,
-    formatDate:formatDate
-  },
-  defaultLayout:'main'
-}));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    helpers: {
+      truncate: truncate,
+      stripTags: stripTags,
+      formatDate: formatDate
+    },
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 app.use(cookieParser());
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // Passport Middleware
 app.use(passport.initialize());
@@ -72,15 +74,15 @@ app.use((req, res, next) => {
 });
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Use Routes
-app.use('/', index);
-app.use('/auth', auth);
-app.use('/stories', stories);
+app.use("/", index);
+app.use("/auth", auth);
+app.use("/stories", stories);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`)
+  console.log(`Server started on http://localhost:${port}`);
 });
